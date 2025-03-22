fe3d8805b4bc04e88d135107c8c1b41559d13528
Minsup Ju <me@ptcookie.net>
2025-03-22 16:24:05 +0900

    chore: Lint and format
    Add ESLint and Prettier.
    Apply formatter.

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import { includeIgnoreFile } from "@eslint/compat";
+import tsEslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const gitignorePath = resolve(__dirname, ".gitignore");
+
+export default defineConfig([
+  includeIgnoreFile(gitignorePath),
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  tsEslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+]);
