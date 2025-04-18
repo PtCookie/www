c3bcd1a0d0b46562cbb5d1ef5b14245472bdea90
Minsup Ju <me@ptcookie.net>
2025-04-18 13:43:43 +0900

    chore(deps): Bump react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react(), mdx()],
+
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [mdx(), react()],
 });
