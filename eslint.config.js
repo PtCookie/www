1e23a9730bd2bfc969419fa32efdd2a8b8056aaf
Minsup Ju <me@ptcookie.net>
2025-04-19 22:51:41 +0900

    style: tailwind format
    Apply tailwindcss prettier plugin.
    Apply ESLint react plugin.

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import globals from "globals";
 import js from "@eslint/js";
 import { includeIgnoreFile } from "@eslint/compat";
 import tsEslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 import eslintPluginAstro from "eslint-plugin-astro";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -24,5 +25,10 @@ export default defineConfig([
     extends: ["js/recommended"],
   },
   tsEslint.configs.recommended,
+  {
+    files: ["**/*.{jsx,tsx}"],
+    settings: { react: { version: "detect" } },
+    ...pluginReact.configs.flat.recommended,
+  },
   ...eslintPluginAstro.configs.recommended,
 ]);
--- prettier.config.js
+++ prettier.config.js
@@ -3,7 +3,7 @@
  * @type {import("prettier").Config}
  */
 const config = {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [
     {
       files: "*.astro",
@@ -12,6 +12,8 @@ const config = {
       },
     },
   ],
+
+  tailwindFunctions: ["clsx", "cn", "cva"],
 };
 
 export default config;
