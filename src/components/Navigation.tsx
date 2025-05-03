cc4708dcdb682f9d01be7480ada8254f5d94da05
Minsup Ju <me@ptcookie.net>
2025-05-03 18:03:33 +0900

    feat: Navigation
    Implement navigation menus.

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu.tsx";
+
+export interface MenuEntry {
+  name: string;
+  link: string;
+}
+
+interface Props {
+  menuEntry: MenuEntry[];
+}
+
+export function Navigation({ menuEntry }: Props) {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {menuEntry.map((item, index) => (
+          <NavigationMenuItem key={index}>
+            <NavigationMenuLink href={item.link}>{item.name}</NavigationMenuLink>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
