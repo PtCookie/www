882e95e6ce007efbb862f46366ef90808fd1a3b1
Minsup Ju <me@ptcookie.net>
2025-05-13 09:15:36 +0900

    feat: Font
    Change font-family.

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <header className="mx-auto my-1.5 flex w-full justify-between border-b-2 px-8 py-2 sm:my-6 sm:py-4">
       <div className="flex items-center">
-        <a className="text-xl font-bold sm:text-3xl sm:leading-tight">{packageJson.name}</a>
+        <a className="font-sans text-xl font-bold sm:text-3xl sm:leading-tight">{packageJson.name}</a>
       </div>
       <div className="flex items-center">
         <ModeToggle />
