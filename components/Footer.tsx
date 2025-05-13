d493c8eb1b696655297d325fdb1a85f8460546dd
Minsup Ju <me@ptcookie.net>
2025-05-13 10:27:27 +0900

    feat: Metadata
    Add favicon and svg icon.
    Add OG images.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,12 @@
+import * as React from "react";
 import packageJson from "@/package.json";
 
 export function Footer() {
   return (
     <footer className="text-muted-foreground mx-auto my-6 flex w-full flex-col gap-4 border-t-2 py-4 text-center font-sans text-sm sm:py-6 sm:text-lg">
-      <p>&copy; 2025, {packageJson.author.name}</p>
+      <p>
+        &copy; {new Date().getFullYear()}, {packageJson.author.name}
+      </p>
     </footer>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { ModeToggle } from "./ModeToggle";
 import packageJson from "@/package.json";
 
@@ -5,7 +6,7 @@ export function Header() {
   return (
     <header className="mx-auto my-1.5 flex w-full justify-between border-b-2 px-8 py-2 sm:my-6 sm:py-4">
       <div className="flex items-center">
-        <a className="font-sans text-xl font-bold sm:text-3xl sm:leading-tight">{packageJson.name}</a>
+        <a className="font-serif text-xl font-bold sm:text-3xl sm:leading-tight">{packageJson.name}</a>
       </div>
       <div className="flex items-center">
         <ModeToggle />
