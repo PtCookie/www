17d967c7bce1cf2334745bb819e3d3d0877343fc
Minsup Ju <me@ptcookie.net>
2025-06-29 02:42:19 +0900

    chore(ver): Dependencies
    Bump dependencies' version.
    Refactor ESLint rules.

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,15 @@
 // @ts-check
+import { resolve } from "node:path";
 import { FlatCompat } from "@eslint/eslintrc";
+import { includeIgnoreFile } from "@eslint/compat";
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
+const eslintConfig = [
+  includeIgnoreFile(resolve(import.meta.dirname, ".gitignore")),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
 
 export default eslintConfig;
